a9ac144abe44c087e1c3fa789366815fa1c8e6b9
farelyudapratama <dhakayudha2@gmail.com>
2024-10-31 20:37:23 +0700

    e

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -3,8 +3,7 @@ import CONFIG from '../../globals/config';
 const createRestaurantDetailTemplate = (restaurant) => `
   <div class="restaurant-detail" tabindex="0" aria-label="Details of restaurant ${restaurant.name}">
     <div class="restaurant-header">
-      <link rel="preload" href="${CONFIG.BASE_IMAGE_URL}large/${restaurant.pictureId}" as="image">
-      <img alt="Image of ${restaurant.name} restaurant" crossorigin="anonymous" loading="lazy" width="448" height="300" src="${CONFIG.BASE_IMAGE_URL}large/${restaurant.pictureId}">
+      <img alt="Image of ${restaurant.name} restaurant" crossorigin="anonymous" loading="lazy" width="448" height="300" src="${CONFIG.BASE_IMAGE_URL}medium/${restaurant.pictureId}">
       <div class="restaurant-info">
         <h2>${restaurant.name}</h2>
         <p class="city" aria-label="City: ${restaurant.city}">${restaurant.city}</p>
