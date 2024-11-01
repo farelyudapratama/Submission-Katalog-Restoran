d7e5ae8d1fe106ba7cfb20d0ea0d9bfc499ebd5c
farelyudapratama <dhakayudha2@gmail.com>
2024-11-01 09:54:21 +0700

    p

--- webpack.common.js
+++ webpack.common.js
@@ -78,6 +78,6 @@ module.exports = {
   },
 };
 
-if (process.env.CI !== 'true') {
-  module.exports.plugins.push(new BundleAnalyzerPlugin());
-}
+// if (process.env.CI !== 'true') {
+//   module.exports.plugins.push(new BundleAnalyzerPlugin());
+// }
